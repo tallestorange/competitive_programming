903e29f946a66dbdb226ddb346de752639ac3605
Yuhel Tanaka <tallestorangeyuhei@gmail.com>
2020-02-14 20:59:45 +0900

    updated

--- problems/ABC131/abc131_e.py
+++ problems/ABC131/abc131_e.py
@@ -1,34 +1,29 @@
 N, K = map(int, input().split())
 
 def f(N, K):
-    for i in range(1, N+1):
-        a = i*(i-1)//2
-        for j in range(1, N+1):
-            b = j
-            for k in range(0, N+1):
-                c = k
-                if i+j+k+1==N and a+b+c==K:
-                    return (i, j, k)
-    return (-1, -1, -1)
+    for k in range(1, N+1):
+        a = k*(k-1)//2
+        for l in range(k):
+            b = l*(l-1)//2
+            if a-b==K and 1+k==N:
+                return (k, l)
 
-i, j, k = f(N, K)
-if i==-1 and j==-1 and k==-1:
+    return (-1, -1)
+
+a, b = f(N, K)
+if a==-1 and b==-1:
     print(-1)
 else:
     l = []
-    a = 1
-    for a in range(2, 2+i):
-        l.append((1,a))
+    v = []
 
-    p = a
-    for a in range(p, p+j):
-        l.append((a, a+1))
-    p = a
+    for i in range(a):
+        l.append((1, i+2))
+        v.append(i+2)
 
-    for a in range(p+1, N+1):
-        for b in range(a+1, N+1):
-            l.append((a,b))
-    
+    for i in range(b-1):
+        l.append((v[i], v[i+1]))
+
     print(len(l))
-    for a, b in l:
-        print(a, b)
+    for i,j in l:
+        print(i, j)
