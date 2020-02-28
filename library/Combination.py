8fe97e9b97633152d47a9b7abc5e9441073e47b3
Yuhel Tanaka <tallestorangeyuhei@gmail.com>
2020-02-28 18:58:57 +0900

    updated

--- library/Combination.py
+++ library/Combination.py
@@ -1,9 +1,14 @@
-mod = 10**9+7
-fac = [1]*(N+1)
-rev = [1]*(N+1)
+MOD = 10**9+7
+n = 10**5
+fac = [1]*(n+1)
+rev = [1]*(n+1)
  
-for i in range(1,N+1):
-  fac[i] = i*fac[i-1]%mod
-  rev[i] = pow(fac[i], mod-2, mod)
+for i in range(1,n+1):
+  fac[i] = i*fac[i-1]%MOD
+  rev[i] = pow(fac[i], MOD-2, MOD)
  
-comb = lambda a,b:(fac[a]*rev[a-b]*rev[b])%mod
+comb = lambda a,b:(fac[a]*rev[a-b]*rev[b])%MOD
+
+
+if __name__ == "__main__":
+  print(comb(6, 2))
