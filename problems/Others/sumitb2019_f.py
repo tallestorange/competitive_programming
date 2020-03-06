745bc7dab4f1b6b6dca7521995f7ebab89f859c0
Yuhel Tanaka <tallestorangeyuhei@gmail.com>
2020-03-06 19:15:56 +0900

    updated

--- problems/Others/sumitb2019_f.py
+++ problems/Others/sumitb2019_f.py
@@ -2,17 +2,38 @@ T1, T2 = map(int, input().split())
 A1, A2 = map(int, input().split())
 B1, B2 = map(int, input().split())
 
-d1 = A1*T1+A2*T2
-d2 = B1*T1+B2*T2
+def check(k):
+    v1 = A1 - B1
+    v2 = A2 - B2
 
-if d1==d2:
-    print("infinity")
-elif A1==B1:
-    print(1)
-else:
-    v1 = A1*T1
-    v2 = B1*T1
-    if (v1-v2<0 and d1-d2<0) or (v1-v2>0 and d1-d2>0):
-        print(0)
+    a, b = k//2, k%2
+    d1 = (a+b)*(v1*T1) + (a)*(v2*T2)
+
+    k += 1
+
+    a, b = k//2, k%2
+    d2 = (a+b)*(v1*T1) + (a)*(v2*T2)
+
+    if d1==0:
+        return True
+    elif d2==0:
+        return True
+    elif (d1<0 and d2>0) or (d1>0 and d2<0):
+        return True
     else:
-        print(1+2*(abs(v1-v2)//abs(d1-d2)))
+        return False
+
+if A1*T1+A2*T2 == B1*T1+B2*T2:
+    print("infinity")
+else:
+    l = -1
+    r = 10**20
+
+    while r-l>1:
+        m = (l+r)//2
+        if check(m):
+            l = m
+        else:
+            r = m
+    
+    print(l)
