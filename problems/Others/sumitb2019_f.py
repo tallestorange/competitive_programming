f9cc2db699479c855fd9420a159402b40ad718cf
Yuhel Tanaka <tallestorangeyuhei@gmail.com>
2020-03-10 01:51:49 +0900

    updated

--- problems/Others/sumitb2019_f.py
+++ problems/Others/sumitb2019_f.py
@@ -1,35 +1,16 @@
 T1, T2 = map(int, input().split())
 A1, A2 = map(int, input().split())
 B1, B2 = map(int, input().split())
+v1, v2 = A1 - B1, A2 - B2
+d1, d2 = v1 * T1, v2 * T2
+if d1 > 0:
+    d1 *= -1
+    d2 *= -1
 
-def check(k):
-    v1 = A1 - B1
-    v2 = A2 - B2
-
-    a, b = k//2, k%2
-    d1 = (a+b)*(v1*T1) + (a)*(v2*T2)
-
-    k += 1
-
-    a, b = k//2, k%2
-    d2 = (a+b)*(v1*T1) + (a)*(v2*T2)
-
-    if d1*d2<=0:
-        return True
-    else:
-        return False
-
-if A1*T1+A2*T2 == B1*T1+B2*T2:
+if d1 + d2 < 0:
+    print(0)
+elif d1 + d2 ==  0:
     print("infinity")
-else:
-    l = -1
-    r = 10**20
-
-    while r-l>1:
-        m = (l+r)//2
-        if check(m):
-            l = m
-        else:
-            r = m
-    
-    print(l)
+elif d1 + d2 > 0:
+    s, t = divmod(-d1, d1+d2)
+    print(2*s+(t!=0))
