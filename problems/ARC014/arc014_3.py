dcb811ed0dca4214b96c916110a02ad7ab04d95e
Yuhel Tanaka <tallestorangeyuhei@gmail.com>
2020-03-18 12:52:21 +0900

    updated

--- problems/ARC014/arc014_3.py
+++ problems/ARC014/arc014_3.py
@@ -1,18 +1,6 @@
-from collections import deque
+from collections import Counter
 
 N = int(input())
 S = input()
-dp = [[[[0]*(N+1) for _ in range(N+1)] for _ in range(4)] fpr _ in range(4)]
-
-for i in range(1, N+1):
-    for j in range(i+1):
-        # for l in range(4):
-        #     for r in range(4):
-        s = S[i-1]
-        if s == "R":
-            dp[i-1][j-2] = dp[i-1][j-1][1]
-            pass
-        elif s == "G":
-            pass
-        elif s == "B":
-            pass
+c = Counter(S)
+print(c["R"]%2+c["G"]%2+c["B"]%2)
